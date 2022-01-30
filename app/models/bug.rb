60a828b594934673916ab40a7084c12aa58690a4
Zafir <zfr.ahmd5661@gmail.com>
2022-01-30 22:36:35 +0500

    added validation on deadline deadline_cannot_be_in_the_past

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -10,10 +10,19 @@ class Bug < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :project_id }, length: {minimum: 6, maximum: 100}
   validates :bug_type, presence: true
   validates :feature_status, :bug_status, presence: true
-  #validates :description, presence: true, length: {minimum: 10, maximum: 500}
+  validates :description, presence: true, length: {minimum: 10, maximum: 500}
   validate :correct_image_type
+  validate :deadline_cannot_be_in_the_past
+  
 
   private
+
+  def deadline_cannot_be_in_the_past
+    if Date.parse(deadline.to_s).past? || deadline.strftime("%I:%M%p") < Time.now.strftime("%I:%M%p")
+      errors.add(:deadline, "can't be in the past")
+    end
+  end
+
   def correct_image_type
     if image.attached? && !image.content_type.in?(%w(image/png image/gif))
       errors.add(:image, 'Must be a png or a gif file')
